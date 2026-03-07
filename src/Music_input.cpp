35c63694ae32cd3d039b15c4b02bd92bf2cbcbee
Sekou <Sekoutraore750@yahoo.com>
2026-03-07 00:55:34 -0500

    Started writing input stream

--- src/Music_input.cpp
+++ src/Music_input.cpp
@@ -9,12 +9,14 @@ using namespace std;
 
 int main() {
     
+     // Initialize PortAudio
      PaError err = Pa_Initialize();
+     // Return an error code if initialization fails
     if (err != paNoError) {
         std::cout << "PortAudio init failed!\n";
         return 1;
     }
-
+ 
     std::cout << "PortAudio initialized beautifully!\n";
 
     int numDevices = Pa_GetDeviceCount();
