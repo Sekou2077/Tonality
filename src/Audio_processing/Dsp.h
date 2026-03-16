096c3925f15e15644f20a96f0f13b37aecbb5dcf
Sekou <Sekoutraore750@yahoo.com>
2026-03-16 00:28:42 -0400

    Added the new header file

---
+++ src/Audio_processing/Dsp.h
@@ -0,0 +1,9 @@
+#ifndef TONALITY_DSP_H // If not defined, define it to prevent multiple inclusions
+#define TONALITY_DSP_H //   Define the header guard
+
+
+
+
+int Dsp(); // Function declaration for the DSP processing function
+
+#endif // TONALITY_DSP_H
