9ab45ddc5c173803bb50893c2a6f22e99ce469a6
Sekou <Sekoutraore750@yahoo.com>
2026-03-19 05:52:18 -0400

    small function return type mismatch

--- src/Audio_processing/Dsp.h
+++ src/Audio_processing/Dsp.h
@@ -1,6 +1,6 @@
 #ifndef TONALITY_DSP_H // If not defined, define it to prevent multiple inclusions
 #define TONALITY_DSP_H //   Define the header guard
 
-int Dsp(std::vector<float>& Recording); // Function declaration for the DSP processing function
+void Dsp(std::vector<float>& Recording); // Function declaration for the DSP processing function
 
 #endif // TONALITY_DSP_H
